6c17e3f2630a95bd0ccc3a3fa0d1e346c1ffb92a
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-02-28 12:49:40 +0530

    Removed the typos and errors from the code

--- index2.js
+++ index2.js
@@ -1,7 +1,7 @@
 const userTab = document.querySelector("[data-userWeather]");
 const searchTab = document.querySelector("[data-searchWeather]");
 const userContainer = document.querySelector(".weather-container");
-const searchForm = document.querySelector(".form-container");
+const searchForm = document.querySelector("[data-searchForm]");
 const grantAccessButton = document.querySelector("[data-AccessBtn]")
 
 const grantAccessContainer = document.querySelector(".grant-location");
@@ -18,12 +18,37 @@ console.log(grantAccessContainer)
 console.log(loadingScreen)
 // The preusable Variables
 
-let currentTab = userTab;
+let oldTab = userTab;
 const api_key = "72a9695b76539a67310f87d0a6681a75";
-const api = "https://api.openweathermap.org/data/3.0/onecall";
-currentTab.classList.add("current-tab");
+
+oldTab.classList.add("current-tab");
 getfromSessionStorage();
 // Adding event listeners to the tabs for switching them
+
+
+function switchTab(newTab)
+{ 
+    if(newTab != oldTab)
+    {
+        oldTab.classList.remove("current-tab");
+        oldTab = newTab
+        oldTab.classList.add("current-tab");
+        
+        // To know what tab we are on we will check the tab: using the active class:
+        if(!searchForm.classList.contains("active")){
+            userInfoContainer.classList.remove("active");
+            
+            grantAccessContainer.classList.remove("active");    searchForm.classList.add("active");
+        }
+        else{
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.add("active");
+            // Now we are in the main container so we need to check the local storage first or we need access:
+            getfromSessionStorage();
+            
+        }
+    }
+}
 userTab.addEventListener("click" , ()=>
 {
     switchTab(userTab);
@@ -35,28 +60,7 @@ searchTab.addEventListener("click" , ()=>
     switchTab(searchTab);
 });
 
-function switchTab(Clicked_tab)
-{ 
-    if(Clicked_tab != currentTab)
-    {
-        currentTab.classList.remove("current-tab");
-        currentTab = Clicked_tab
-        currentTab.classList.add("current-tab");
-    }
 
-    // To know what tab we are on we will check the tab: using the active class:
-    if(!searchForm.classList.contains("active")){
-        searchForm.classList.add("active");
-        userInfoContainer.classList.remove("active");
-    }
-    else{
-        searchForm.classList.remove("active");
-        userInfoContainer.classList.add("active");
-        // Now we are in the main container so we need to check the local storage first or we need access:
-        getfromSessionStorage();
-
-    }
-}
 
 // It checks if coordinates are already in session storage:
 function getfromSessionStorage()
@@ -76,7 +80,7 @@ function getfromSessionStorage()
 // now fucntion fetchuserinfo fetches the user data"
 async function fetchUserWeatherInfo(coordinates)
 {
-    const{ lat , lng  }= coordinates;
+    const {lat, lon} = coordinates;
 // To show the loader we need to disappear the grantAcess container first then 
     grantAccessContainer.classList.remove("active");  
 // Now we need to display the loader:   
@@ -84,17 +88,18 @@ async function fetchUserWeatherInfo(coordinates)
 
 // API CALL: Now we will fetch the infor using the api call :
 try{
-    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}
     `); 
     const resultData = await result.json(); 
     loadingScreen.classList.remove("active");
     userInfoContainer.classList.add("active");
-
+    console.log(resultData);
     // Now we need to render weather info to the Ui:
     RenderDataUI(resultData);
 }
 
 catch(er){
+    loadingScreen.classList.remove("active");
     // homework...
 }
 }
@@ -114,7 +119,7 @@ function RenderDataUI(weatherInfo){
     const clouds = document.querySelector("[data-clouds]")
 
     cityName.innerText = weatherInfo?.name;
-    countryIcon.src = `https://flagcon.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`
+    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`
     desc.innerText = weatherInfo?.weather[0]?.description;
     weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
     weatherTemp.innerText  = weatherInfo?.main?.temp;
@@ -137,44 +142,60 @@ function getLocation()
     }
 }
 
-function showPosition(position)
-{
+function showPosition(position) {
+
     const userCoordinates = {
         lat: position.coords.latitude,
         lon: position.coords.longitude,
     }
-    sessionStorage.setItem("user-coordinates" , JSON.stringify(userCoordinates));
+   
+    console.log(userCoordinates);
+    sessionStorage.setItem("user-coordinates", JSON.stringify(userCoordinates));
     fetchUserWeatherInfo(userCoordinates);
-} 
 
+}
+
+
+
+
+
+
+
+
+// The search tab functionality : code:
 
 const searchInput = document.querySelector("[data-searchInput]");
 searchForm.addEventListener("submit", (e)=>{
     e.preventDefault();
-    let cityName = searchInput.ariaValueMax;
-    if(cityName =="") return;
+    let cityName = searchInput.value;
+    console.log(cityName);
+    if(cityName === "") return; 
     else{
-        fetchSearchWeatherInfo(cityName);
+fetchSearchWeatherInfo(cityName);
     }
 })
-async function fetchSearchWeatherInfo(){
 
-    loadingScreen.classList.add("active");
+
+async function fetchSearchWeatherInfo(city){
+
+    console.log(city);
+loadingScreen.classList.add("active");
     userInfoContainer.classList.remove("active");
     grantAccessContainer.classList.remove("active");
 
     try {
-        const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+        const response = await fetch(      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
           );
-        data = await response.JSON();
-        loadingScreen.classList.remove("active");
+       const data = await response.json();
+        
+       console.log(data);
+    loadingScreen.classList.remove("active");
         userInfoContainer.classList.add("active");
         RenderDataUI(data);
 
     }
     catch(err){
-        //
+        console.log(err);
     }
 }
 
